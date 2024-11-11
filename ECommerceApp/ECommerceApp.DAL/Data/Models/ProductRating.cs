6ff5702ce5aed5536f1e7b4bda9ffad21dd6cb3b
Vladislav <vladislav.d.nikov04@gmail.com>
2024-11-11 15:06:53 +0200

    Add ProductRating model
    Implement many-to-many ProductRating model. Create new Db migration for it.
    
    Update Product and ApplicationUser.

---
+++ ECommerceApp/ECommerceApp.DAL/Data/Models/ProductRating.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceApp.DAL.Data.Models
+{
+    public class ProductRating
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public Guid UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public int Rating { get; set; }
+    }
+}
